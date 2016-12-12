fe1d53dc8de42ace2ce4c46375761a5aa31e3521
vmdharan <rexar7@gmail.com>
2016-12-12 18:27:03 +1100

    Move debug code into a function
    Move debug code into a function, reset register values and create the program loop.

--- src/exec.js
+++ src/exec.js
@@ -7,57 +7,80 @@ console.log(cpu);
  */
 
 // General purpose registers
-var rAX = 0x0001;
-var rBX = 0x0010;
-var rCX = 0x0100;
-var rDX = 0x1000;
+var rAX = 0x0000;
+var rBX = 0x0000;
+var rCX = 0x0000;
+var rDX = 0x0000;
 
 // Index registers
-var rSI = 0x000a;		// Source Index
-var rDI = 0x00b0;		// Destination Index
-var rBP = 0x0c00;		// Base Pointer
-var rSP = 0xd000;		// Stack Pointer
+var rSI = 0x0000;		// Source Index
+var rDI = 0x0000;		// Destination Index
+var rBP = 0x0000;		// Base Pointer
+var rSP = 0x0000;		// Stack Pointer
 
 // Segment registers
-var rCS = 0x000f;		// Code Segment
-var rDS = 0x00f0;		// Data Segment
-var rES = 0x0f00;		// Extra Segment
-var rSS = 0xf000;		// Stack Segment
+var rCS = 0x0000;		// Code Segment
+var rDS = 0x0000;		// Data Segment
+var rES = 0x0000;		// Extra Segment
+var rSS = 0x0000;		// Stack Segment
 
 // Program counter
-var rPC = 0xFFFF;		// Instruction pointer
+var rPC = 0x0000;		// Instruction pointer
 
 // Status register
 var rST = 0x0000;
 
+
+
+// Run program
+run8086();
+
+
+// Main program loop
+function run8086() {
+	
+	// Fetch-Decode-Execute cycle
+	while(true) {
+		
+		debugRegisters();
+		
+		rPC = 0x0001;
+		
+		debugRegisters();
+		break;
+	}
+}
+
 // Debugging
-console.log('--------------------');
-console.log('General purpose registers');
-console.log('rAX: 0x' + ('0000' + rAX.toString(16)).slice(-4));
-console.log('rBX: 0x' + ('0000' + rBX.toString(16)).slice(-4));
-console.log('rCX: 0x' + ('0000' + rCX.toString(16)).slice(-4));
-console.log('rDX: 0x' + ('0000' + rDX.toString(16)).slice(-4));
-console.log('--------------------');
+function debugRegisters() {
+	console.log('--------------------');
+	console.log('General purpose registers');
+	console.log('rAX: 0x' + ('0000' + rAX.toString(16)).slice(-4));
+	console.log('rBX: 0x' + ('0000' + rBX.toString(16)).slice(-4));
+	console.log('rCX: 0x' + ('0000' + rCX.toString(16)).slice(-4));
+	console.log('rDX: 0x' + ('0000' + rDX.toString(16)).slice(-4));
+	console.log('--------------------');
 
-console.log('Index registers');
-console.log('rSI: 0x' + ('0000' + rSI.toString(16)).slice(-4));
-console.log('rDI: 0x' + ('0000' + rDI.toString(16)).slice(-4));
-console.log('rBP: 0x' + ('0000' + rBP.toString(16)).slice(-4));
-console.log('rSP: 0x' + ('0000' + rSP.toString(16)).slice(-4));
-console.log('--------------------');
+	console.log('Index registers');
+	console.log('rSI: 0x' + ('0000' + rSI.toString(16)).slice(-4));
+	console.log('rDI: 0x' + ('0000' + rDI.toString(16)).slice(-4));
+	console.log('rBP: 0x' + ('0000' + rBP.toString(16)).slice(-4));
+	console.log('rSP: 0x' + ('0000' + rSP.toString(16)).slice(-4));
+	console.log('--------------------');
 
-console.log('Segment registers');
-console.log('rCS: 0x' + ('0000' + rCS.toString(16)).slice(-4));
-console.log('rDS: 0x' + ('0000' + rDS.toString(16)).slice(-4));
-console.log('rES: 0x' + ('0000' + rES.toString(16)).slice(-4));
-console.log('rSS: 0x' + ('0000' + rSS.toString(16)).slice(-4));
-console.log('--------------------');
+	console.log('Segment registers');
+	console.log('rCS: 0x' + ('0000' + rCS.toString(16)).slice(-4));
+	console.log('rDS: 0x' + ('0000' + rDS.toString(16)).slice(-4));
+	console.log('rES: 0x' + ('0000' + rES.toString(16)).slice(-4));
+	console.log('rSS: 0x' + ('0000' + rSS.toString(16)).slice(-4));
+	console.log('--------------------');
 
-console.log('Program counter');
-console.log('rPC: 0x' + ('0000' + rPC.toString(16)).slice(-4));
-console.log('--------------------');
+	console.log('Program counter');
+	console.log('rPC: 0x' + ('0000' + rPC.toString(16)).slice(-4));
+	console.log('--------------------');
 
-console.log('Status register');
-console.log('rST: 0x' + ('0000' + rST.toString(16)).slice(-4));
-console.log('--------------------');
+	console.log('Status register');
+	console.log('rST: 0x' + ('0000' + rST.toString(16)).slice(-4));
+	console.log('--------------------');
+}
 
