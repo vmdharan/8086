e889e1564b0ce8e5e4590d35de2809f57ceb8f96
vmdharan <rexar7@gmail.com>
2016-12-13 15:33:56 +1100

    Handling flags in the status register
    - Wrote code to handle flags in the status register.
    - Added a function for debugging the status register while setting and
    resetting specific flags.

--- src/exec.js
+++ src/exec.js
@@ -82,6 +82,9 @@ function run8086() {
 		rBX = ram8086[97];
 		
 		debugRegisters();
+		
+		debugStatusRegister();
+		
 		break;
 	}
 }
@@ -113,5 +116,146 @@ function debugRegisters() {
 	console.log(rCS_str + ' ' + rDS_str + ' ' + rES_str + ' ' + rSS_str);
 	console.log(rIP_str + ' ' + rST_str);
 	console.log('--------------------');
+}
+
+// Return the value of the flag provided as the parameter.
+function getFlag(varFL) {
+	var retVal = varFL;
+	
+	switch (varFL) {
+	case 'OF': retVal = ((rST & 0x0800) >> 11); break;
+	case 'DF': retVal = ((rST & 0x0400) >> 10); break;
+	case 'IF': retVal = ((rST & 0x0200) >> 9); break;
+	case 'TF': retVal = ((rST & 0x0100) >> 8); break;
+	case 'SF': retVal = ((rST & 0x0080) >> 7); break;
+	case 'ZF': retVal = ((rST & 0x0040) >> 6); break;
+	case 'AF': retVal = ((rST & 0x0010) >> 4); break;
+	case 'PF': retVal = ((rST & 0x0004) >> 2); break;
+	case 'CF': retVal = (rST & 0x0001); break;
+	default: retVal = (rST & 0x0000); break;
+	}
+	
+	return retVal;
+}
+
+// Set the value of the flag provided as the parameter.
+function setFlag(varFL, val) {
+	// If this is a set operation, then perform a boolean OR operation 
+	// with the status register 
+	if(val == 1) {
+		switch (varFL) {
+		case 'OF': rST = rST | ((val << 11) & 0x0800); break;
+		case 'DF': rST = rST | ((val << 10) & 0x0400); break;
+		case 'IF': rST = rST | ((val << 9) & 0x0200); break;
+		case 'TF': rST = rST | ((val << 8) & 0x0100); break;
+		case 'SF': rST = rST | ((val << 7) & 0x0080); break;
+		case 'ZF': rST = rST | ((val << 6) & 0x0040); break;
+		case 'AF': rST = rST | ((val << 4) & 0x0010); break;
+		case 'PF': rST = rST | ((val << 2) & 0x0004); break;
+		case 'CF': rST = rST | (val & 0x0001); break;
+		default: break;
+		}
+	}
+	// If this is a reset operation, then zero out the flag.
+	else {
+		switch (varFL) {
+		case 'OF': rST = rST & 0xa7ff; break;
+		case 'DF': rST = rST & 0xfbff; break;
+		case 'IF': rST = rST & 0xfdff; break;
+		case 'TF': rST = rST & 0xfeff; break;
+		case 'SF': rST = rST & 0xff7f; break;
+		case 'ZF': rST = rST & 0xffbf; break;
+		case 'AF': rST = rST & 0xffef; break;
+		case 'PF': rST = rST & 0xfffb; break;
+		case 'CF': rST = rST & 0xfffe; break;
+		default: break;
+		}
+	}
+}
+
+// Test the status register.
+function debugStatusRegister() {
+	console.log('[LOG] Running tests on status register.')
+	console.log('--------------------');
+	
+	console.log('[LOG] Set OF');
+	setFlag('OF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Set DF');
+	setFlag('DF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Set IF');
+	setFlag('IF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Set TF');
+	setFlag('TF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Set SF');
+	setFlag('SF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Set ZF');
+	setFlag('ZF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Set AF');
+	setFlag('AF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Set PF');
+	setFlag('PF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Set CF');
+	setFlag('CF', 1);
+	console.log(getStatusRegisterString());
+	
+	console.log('--------------------');
+	
+	console.log('[LOG] Reset OF');
+	setFlag('OF', 0);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Reset DF');
+	setFlag('DF', 0);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Reset IF');
+	setFlag('IF', 0);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Reset TF');
+	setFlag('TF', 0);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Reset SF');
+	setFlag('SF', 0);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Reset ZF');
+	setFlag('ZF', 0);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Reset AF');
+	setFlag('AF', 0);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Reset PF');
+	setFlag('PF', 0);
+	console.log(getStatusRegisterString());
+	
+	console.log('[LOG] Reset CF');
+	setFlag('CF', 0);
+	console.log(getStatusRegisterString());
+
+	console.log('--------------------');
+}
+
+function getStatusRegisterString() {
+	return 'rST: 0x' + ('0000' + rST.toString(16)).slice(-4);
 }
 
