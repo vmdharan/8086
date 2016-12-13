1ebb34dea6f35c54e68a308f53ff5b771047c21b
vmdharan <rexar7@gmail.com>
2016-12-13 16:54:53 +1100

    Enumerate instruction set for the 8086
    - Enumerate instruction set for the 8086.
    - Comment out some dubious / duplicate instructions.
    - Move testing code to a separate function.

--- src/exec.js
+++ src/exec.js
@@ -63,59 +63,971 @@ var ram8086 = new Array(I8086_MEMORY_SIZE).fill(0xff);
 // Run program
 run8086();
 
-// Main program loop
+// Fetch-Decode-Execute cycle
 function run8086() {
 	
-	// Fetch-Decode-Execute cycle
+	// Uncomment for testing.
+	//runTest1();
+	
+	// Get the first instruction.
+	var instruction = ram8086[rIP];
+	
 	while(true) {
 		
-		debugRegisters();
+		// Decode and execute the instruction.
+		decode(instruction);
 		
-		console.log('[LOG] Increasing IP by 1');
-		rIP = 0x0001;
-		
-		console.log('[LOG] Set the memory index 96 to have value 0x32.')
-		ram8086[96] = 0x32;
-		
-		console.log('[LOG] Show memory index 96 in rAX and 97 in rBX.')
-		rAX = ram8086[96];
-		rBX = ram8086[97];
-		
-		debugRegisters();
-		
-		debugStatusRegister();
-		
-		break;
+		// Fetch the next instruction from memory.
+		instruction = ram8086[rIP];
 	}
 }
 
-// Display the state of all registers for debugging.
-function debugRegisters() {
+function decode(instruction) {
 	
-	var rAX_str = 'rAX: 0x' + ('0000' + rAX.toString(16)).slice(-4);
-	var rBX_str = 'rBX: 0x' + ('0000' + rBX.toString(16)).slice(-4);
-	var rCX_str = 'rCX: 0x' + ('0000' + rCX.toString(16)).slice(-4);
-	var rDX_str = 'rDX: 0x' + ('0000' + rDX.toString(16)).slice(-4);
+	switch (instruction) {
 	
-	var rSI_str = 'rSI: 0x' + ('0000' + rSI.toString(16)).slice(-4);
-	var rDI_str = 'rDI: 0x' + ('0000' + rDI.toString(16)).slice(-4);
-	var rBP_str = 'rBP: 0x' + ('0000' + rBP.toString(16)).slice(-4);
-	var rSP_str = 'rSP: 0x' + ('0000' + rSP.toString(16)).slice(-4);
+	/*
+	 * Data Transfer
+	 */
 	
-	var rCS_str = 'rCS: 0x' + ('0000' + rCS.toString(16)).slice(-4);
-	var rDS_str = 'rDS: 0x' + ('0000' + rDS.toString(16)).slice(-4);
-	var rES_str = 'rES: 0x' + ('0000' + rES.toString(16)).slice(-4);
-	var rSS_str = 'rSS: 0x' + ('0000' + rSS.toString(16)).slice(-4);
-	
-	var rIP_str = 'rIP: 0x' + ('0000' + rIP.toString(16)).slice(-4);
-	var rST_str = 'rST: 0x' + ('0000' + rST.toString(16)).slice(-4);
-	
-	console.log('--------------------');
-	console.log(rAX_str + ' ' + rBX_str + ' ' + rCX_str + ' ' + rDX_str);
-	console.log(rSI_str + ' ' + rDI_str + ' ' + rBP_str + ' ' + rSP_str);
-	console.log(rCS_str + ' ' + rDS_str + ' ' + rES_str + ' ' + rSS_str);
-	console.log(rIP_str + ' ' + rST_str);
-	console.log('--------------------');
+	// MOV - Move
+	// Register/memory to/from register.
+	case 0x88:
+		break;
+	case 0x89:
+		break;
+	case 0x8A:
+		break;
+	case 0x8B:
+		break;
+
+	// Immediate to register/memory.
+	case 0xC6:
+		break;
+	case 0xC7:
+		break;
+
+	// Immediate to register.
+	case 0xB0:
+		break;
+	case 0xB1:
+		break;
+	case 0xB2:
+		break;
+	case 0xB3:
+		break;
+	case 0xB4:
+		break;
+	case 0xB5:
+		break;
+	case 0xB6:
+		break;
+	case 0xB7:
+		break;
+	case 0xB8:
+		break;
+	case 0xB9:
+		break;
+	case 0xBA:
+		break;
+	case 0xBB:
+		break;
+	case 0xBC:
+		break;
+	case 0xBD:
+		break;
+	case 0xBE:
+		break;
+	case 0xBF:
+		break;
+
+	// Memory to accumulator.
+	// Single byte
+	case 0xA0:  
+		break;
+	// Two bytes
+	case 0xA1: 
+		break;
+
+	// Accumulator to memory.
+	case 0xA2:
+		break;
+	case 0xA3:
+		break;
+
+	// Register/memory to segment register.
+	case 0x87:
+		break;
+
+	// Segment register to register/memory.
+	case 0x8C:
+		break;
+
+	// PUSH - Push.
+	// Register/memory.
+	case 0xFF:
+		break;
+
+	// Register.
+	case 0x50:
+		break;
+	case 0x51:
+		break;
+	case 0x52:
+		break;
+	case 0x53:
+		break;
+	case 0x54:
+		break;
+	case 0x55:
+		break;
+	case 0x56:
+		break;
+	case 0x57:
+		break;
+
+	// Segment register.
+	case 0x06:
+		break;
+	case 0x07:
+		break;
+	case 0x16:
+		break;
+	case 0x17:
+		break;
+
+	// POP - Pop.
+	// Register/memory.
+	case 0x8F:
+		break;
+
+	// Register.
+	case 0x58:
+		break;
+	case 0x59:
+		break;
+	case 0x5A:
+		break;
+	case 0x5B:
+		break;
+	case 0x5C:
+		break;
+	case 0x5D:
+		break;
+	case 0x5E:
+		break;
+	case 0x5F:
+		break;
+
+	// Segment register.
+	//case 0x07:  break;
+	case 0x0F:
+		break;
+	//case 0x17:  break;
+	case 0x1F:
+		break;
+
+	// XCHNG - Exchange.
+	// Register/memory with register.
+	case 0x86:
+		break;
+	//case 0x87:  break;
+
+	// Register with accumulator.
+
+	// Special case when register is Accumulator - NOOP instruction
+	case 0x90:
+		break;
+
+	case 0x91:
+		break;
+	case 0x92:
+		break;
+	case 0x93:
+		break;
+	case 0x94:
+		break;
+	case 0x95:
+		break;
+	case 0x96:
+		break;
+	case 0x97:
+		break;
+
+	// IN - Input to AL/AX from
+	// Fixed port.
+	case 0xE4:
+		break;
+	case 0xE5:
+		break;
+
+	// Variable port (DX).
+	case 0xEC:
+		break;
+	case 0xED:
+		break;
+
+	// OUT - Output from AL/AX to
+	// Fixed port.
+	case 0xE6:
+		break;
+	case 0xE7:
+		break;
+
+	// Variable port (DX).
+	case 0xEE:
+		break;
+	case 0xEF:
+		break;
+
+	// XLAT - Translate byte to AL.
+	case 0xD7:
+		break;
+
+	// LEA - Load EA to register.
+	case 0x8D:
+		break;
+
+	// LDS - Load pointer to DS.
+	case 0xC5:
+		break;
+
+	// LES - Load pointer to ES.
+	case 0xC4:
+		break;
+
+	// LAHF - Load AH with flags.
+	case 0x9F:
+		break;
+
+	// SAHF - Store AH into flags.
+	case 0x9E:
+		break;
+
+	// PUSHF - Push flags.
+	case 0x9C:
+		break;
+
+	// POPF - Pop flags.
+	case 0x9D:
+		break;
+
+
+	/*
+	 * Arithmetic
+	 */
+
+	// ADD - Add
+	// Register/memory with register to either.
+	case 0x00:
+		break;
+	case 0x01:
+		break;
+	case 0x02:
+		break;
+	case 0x03:
+		break;
+
+	// Immediate to register/memory.
+	case 0x80:
+		break;
+	case 0x81:
+		break;
+	case 0x82:
+		break;
+	case 0x83:
+		break;
+
+	// Immediate to accumulator.
+	case 0x04:
+		break;
+	case 0x05:
+		break;
+
+	// ADC - Add with carry
+	// Register/memory with register to either.
+	case 0x10:
+		break;
+	case 0x11:
+		break;
+	case 0x12:
+		break;
+	case 0x13:
+		break;
+
+	// Immediate to register/memory.
+	/*case 0x80:
+		break;
+	case 0x81:
+		break;
+	case 0x82:
+		break;
+	case 0x83:
+		break;
+	*/
+
+	// Immediate to accumulator.
+	case 0x14:
+		break;
+	case 0x15:
+		break;
+
+	// INC - Increment
+	// Register/memory
+	case 0xFE:
+		break;
+	/*case 0xFF:
+		break;
+	*/
+
+	// Register
+	case 0x40:
+		break;
+	case 0x41:
+		break;
+	case 0x42:
+		break;
+	case 0x43:
+		break;
+	case 0x44:
+		break;
+	case 0x45:
+		break;
+	case 0x46:
+		break;
+	case 0x47:
+		break;
+
+	// AAA - ASCII adjust for add
+	case 0x37:
+		break;
+
+	// DAA - Decimal adjust for add
+	case 0x27:
+		break;
+
+	// SUB - Subtract
+	// Register/memory and register to either.
+	case 0x28:
+		break;
+	case 0x29:
+		break;
+	case 0x2A:
+		break;
+	case 0x2B:
+		break;
+
+	// Immediate from register/memory.
+	/*case 0x80:
+		break;
+	case 0x81:
+		break;
+	case 0x82:
+		break;
+	case 0x83:
+		break;
+	*/
+
+	// Immediate from accumulator.
+	case 0x2C:
+		break;
+	case 0x2D:
+		break;
+
+	// SBB - Subtract with borrow.
+	// Register/memory and register to either.
+	case 0x18:
+		break;
+	case 0x19:
+		break;
+	case 0x1A:
+		break;
+	case 0x1B:
+		break;
+
+	// Immediate from register/memory.
+	/*case 0x80:
+		break;
+	case 0x81:
+		break;
+	case 0x82:
+		break;
+	case 0x83:
+		break;
+	*/
+
+	// Immediate from accumulator
+	case 0x1C:
+		break;
+	case 0x1D:
+		break;
+
+	// DEC - Decrement
+	// Register/memory.
+	/*case 0xFE:
+		break;
+	case 0xFF:
+		break;
+	*/
+
+	// Register.
+	case 0x48:
+		break;
+	case 0x49:
+		break;
+	case 0x4A:
+		break;
+	case 0x4B:
+		break;
+	case 0x4C:
+		break;
+	case 0x4D:
+		break;
+	case 0x4E:
+		break;
+	case 0x4F:
+		break;
+
+	// NEG - Change sign
+	case 0xF6:
+		break;
+	case 0xF7:
+		break;
+
+	// CMP - Compare
+	// Register/memory and register.
+	case 0x38:
+		break;
+	case 0x39:
+		break;
+	case 0x3A:
+		break;
+	case 0x3B:
+		break;
+
+	// Immediate with register/memory.
+	/*case 0x80:
+		break;
+	case 0x81:
+		break;
+	case 0x82:
+		break;
+	case 0x83:
+		break;
+	*/
+
+	// Immediate with accumulator.
+	case 0x3C:
+		break;
+	case 0x3D:
+		break;
+
+	// AAS - ASCII adjust for subtract.
+	case 0x3F:
+		break;
+
+	// DAS - Decimal adjust for subtract.
+	case 0x2F:
+		break;
+
+	// MUL - Multiply (unsigned)
+	/*case 0xF6:
+		break;
+	case 0xF7:
+		break;
+	*/
+
+	// IMUL - Integer multiply (signed)
+	/*case 0xF6:
+		break;
+	case 0xF7:
+		break;
+	*/
+
+	// AAM - ASCII adjust for multiply.
+	case 0xD4:
+		break;
+
+	// DIV - Divide (unsigned)
+	/*case 0xF6:
+		break;
+	case 0xF7:
+		break;
+	*/
+
+	// IDIV - Integer divide (signed)
+	/*case 0xF6:
+		break;
+	case 0xF7:
+		break;
+	*/
+
+	// AAD - ASCII adjust for divide.
+	case 0xD5:
+		break;
+
+	// CBW - Convert byte to word.
+	case 0x98:
+		break;
+
+	// CWD - Convert word to double word.
+	case 0x99:
+		break;
+
+
+	/*
+	 * Logic
+	 */
+
+	// NOT - Invert
+	/*case 0xF6:
+		break;
+	case 0xF7:
+		break;
+	*/
+
+	// SHL/SAL - Shift logical/arithmetic left.
+	case 0xD0:
+		break;
+	case 0xD1:
+		break;
+	case 0xD2:
+		break;
+	case 0xD3:
+		break;
+
+	// SHR - Shift logical right.
+	/*case 0xD0:
+		break;
+	case 0xD1:
+		break;
+	case 0xD2:
+		break;
+	case 0xD3:
+		break;
+	*/
+
+	// SAR - Shift arithmetic right.
+	/*case 0xD0:
+		break;
+	case 0xD1:
+		break;
+	case 0xD2:
+		break;
+	case 0xD3:
+		break;
+	*/
+
+	// ROL - Rotate left.
+	/*case 0xD0:
+		break;
+	case 0xD1:
+		break;
+	case 0xD2:
+		break;
+	case 0xD3:
+		break;
+	*/
+
+	// ROR - Rotate right.
+	/*case 0xD0:
+		break;
+	case 0xD1:
+		break;
+	case 0xD2:
+		break;
+	case 0xD3:
+		break;
+	*/
+
+	// RCL - Rotate through carry left.
+	/*case 0xD0:
+		break;
+	case 0xD1:
+		break;
+	case 0xD2:
+		break;
+	case 0xD3:
+		break;
+	*/
+
+	// RCR - Rotate through carry right.
+	/*case 0xD0:
+		break;
+	case 0xD1:
+		break;
+	case 0xD2:
+		break;
+	case 0xD3:
+		break;
+	*/
+
+	// AND - And.
+	// Register/memory and register to either.
+	case 0x20:
+		break;
+	case 0x21:
+		break;
+	case 0x22:
+		break;
+	case 0x23:
+		break;
+
+	// Immediate to register/memory.
+	/*case 0x80:
+		break;
+	case 0x81:
+		break;
+	*/
+
+	// Immmediate to accumulator.
+	case 0x24:
+		break;
+	case 0x25:
+		break;
+
+	// TEST - Add function to flags, no result.
+	// Register/memory and register.
+	case 0x84:
+		break;
+	case 0x85:
+		break;
+
+	// Immediate data and register/memory.
+	/*case 0xF6:
+		break;
+	case 0xF7:
+		break;
+	*/
+
+	// Immediate data and accumulator.
+	case 0xA8:
+		break;
+	case 0xA9:
+		break;
+
+	// OR - Or.
+	// Register/memory and register to either.
+	case 0x08:
+		break;
+	case 0x09:
+		break;
+	case 0x0A:
+		break;
+	case 0x0B:
+		break;
+
+	// Immediate to register/memory.
+	/*case 0x80:
+		break;
+	case 0x81:
+		break;
+	*/
+
+	// Immediate to accumulator.
+	case 0x0C:
+		break;
+	case 0x0D:
+		break;
+
+	// XOR - Exclusive or.
+	// Register/memory and register to either.
+	case 0x30:
+		break;
+	case 0x31:
+		break;
+	case 0x32:
+		break;
+	case 0x33:
+		break;
+
+	// Immediate to register/memory.
+	/*case 0x80:
+		break;
+	case 0x81:
+		break;
+	*/
+
+	// Immediate to accumulator.
+	case 0x34:
+		break;
+	case 0x35:
+		break;
+
+
+	/*
+	 * String manipulation
+	 */
+
+	// REP - Repeat.
+	case 0xF2:
+		break;
+	case 0xF3:
+		break;
+
+	// MOVS - Move String.
+	case 0xA4:
+		break;
+	case 0xA5:
+		break;
+
+	// CMPS - Compare String.
+	case 0xA6:
+		break;
+	case 0xA7:
+		break;
+
+	// SCAS - Scan String.
+	case 0xAE:
+		break;
+	case 0xAF:
+		break;
+
+	// LODS - Load String.
+	case 0xAC:
+		break;
+	case 0xAD:
+		break;
+
+	// STOS - Store String.
+	case 0xAA:
+		break;
+	case 0xAB:
+		break;
+
+
+	/*
+	 * Control Transfer
+	 */
+
+	// CALL - Call.
+	// Direct within segment.
+	case 0xE8:
+		break;
+
+	// Indirect within segment.
+	/*case 0xFF:
+		break;
+	*/
+
+	// Direct intersegment.
+	case 0x9A:
+		break;
+
+	// Indirect intersegment.
+	/*case 0xFF:
+		break;
+	*/
+
+	// JMP - Unconditional Jump.
+	// Direct within segment.
+	case 0xE9:
+		break;
+
+	// Direct within segment-short.
+	case 0xEB:
+		break;
+
+	// Indirect within segment.
+	/*case 0xFF:
+		break;
+	*/
+
+		// Direct intersegment.
+	case 0xEA:
+		break;
+
+	// Indirect intersegment.
+	/*case 0xFF:
+		break;
+	*/
+
+	// RET - Return from CALL.
+	// Within segment.
+	case 0xC3:
+		break;
+
+	// Within segment adding immediate to SP.
+	case 0xC2:
+		break;
+
+	// Intersegment.
+	case 0xCB:
+		break;
+
+	// Intersegment, adding immediate to SP.
+	case 0xCA:
+		break;
+
+	// JE/JZ - Jump on equal/zero.
+	case 0x74:
+		break;
+
+	// JL/JNGE - Jump on less/not greater or equal.
+	case 0x7C:
+		break;
+
+	// JLE/JNG - Jump on less or equal/not greater.
+	case 0x7E:
+		break;
+
+	// JB/JNAE - Jump on below/not above or equal.
+	case 0x72:
+		break;
+
+	// JBE/JNA - Jump on below or equal/not above.
+	case 0x76:
+		break;
+
+	// JP/JPE - Jump on parity/parity even.
+	case 0x7A:
+		break;
+
+	// JO - Jump on overflow.
+	case 0x70:
+		break;
+
+	// JS - Jump on sign.
+	case 0x78:
+		break;
+
+	// JNE/JNZ - Jump on not equal/not zero.
+	case 0x75:
+		break;
+
+	// JNL/JGE - Jump on not less/greater or equal.
+	case 0x7D:
+		break;
+
+	// JNLE/JG - Jump on not less or equal/greater.
+	case 0x7F:
+		break;
+
+	// JNB/JAE - Jump on not below/above or equal.
+	case 0x73:
+		break;
+
+	// JNBE/JA - Jump on not below or equal/above.
+	case 0x77:
+		break;
+
+	// JNP/JPO - Jump on not parity/parity odd.
+	case 0x7B:
+		break;
+
+	// JNO - Jump on not overflow.
+	case 0x71:
+		break;
+
+	// JNS - Jump on not sign.
+	case 0x79:
+		break;
+
+	// LOOP - Loop CX times.
+	case 0xE2:
+		break;
+
+	// LOOPZ/LOOPE - Loop while zero/equal.
+	case 0xE1:
+		break;
+
+	// LOOPNZ/LOOPNE - Loop while not zero/not equal.
+	case 0xE0:
+		break;
+
+	// JCXZ - Jump on CX zero.
+	case 0xE3:
+		break;
+
+	// INT - Interrupt.
+	// Type specified.
+	case 0xCD:
+		break;
+
+	// Type 3.
+	case 0xCC:
+		break;
+
+	// INTO - Interrupt on overflow.
+	case 0xCE:
+		break;
+
+	// IRET - Interrupt return.
+	case 0xCF:
+		break;
+
+
+	/*
+	 * Processor Control
+	 */
+
+	//CLC - Clear carry
+	case 0xF8:  
+		break;
+
+	// CMC - Complement carry
+	case 0xF5:
+		break;
+
+	// CLD - Clear direction
+	case 0xFC:  
+		break;
+
+	// CLI - Clear interrupt
+	case 0xFA:  
+		break;
+
+	// HLT - Halt
+	case 0xF4:
+		break;
+
+	// LOCK - Bus lock prefix
+	case 0xF0:
+		break;
+
+	// STC - Set carry
+	case 0xF9:  
+		break;
+
+	// NOP - No operation
+	/*case 0x90:
+		break;
+	*/
+
+	// STD - Set direction
+	case 0xFD:  
+		break;
+
+	// STI - Set Interrupt
+	case 0xFB:  
+		break;
+
+	// WAIT - Wait
+	case 0x9B:
+		break;
+
+	// ESC - Escape (to external device)
+	case 0xD8:
+		break;
+	case 0xD9:
+		break;
+	case 0xDA:
+		break;
+	case 0xDB:
+		break;
+	case 0xDC:
+		break;
+	case 0xDD:
+		break;
+	case 0xDE:
+		break;
+	case 0xDF:
+		break;
+
+	default:
+		break;
+
+	}
 }
 
 // Return the value of the flag provided as the parameter.
@@ -171,6 +1083,52 @@ function setFlag(varFL, val) {
 		default: break;
 		}
 	}
+}
+
+function runTest1() {
+	debugRegisters();
+	
+	console.log('[LOG] Increasing IP by 1');
+	rIP = 0x0001;
+	
+	console.log('[LOG] Set the memory index 96 to have value 0x32.')
+	ram8086[96] = 0x32;
+	
+	console.log('[LOG] Show memory index 96 in rAX and 97 in rBX.')
+	rAX = ram8086[96];
+	rBX = ram8086[97];
+	
+	debugRegisters();
+	debugStatusRegister();
+}
+
+//Display the state of all registers for debugging.
+function debugRegisters() {
+	
+	var rAX_str = 'rAX: 0x' + ('0000' + rAX.toString(16)).slice(-4);
+	var rBX_str = 'rBX: 0x' + ('0000' + rBX.toString(16)).slice(-4);
+	var rCX_str = 'rCX: 0x' + ('0000' + rCX.toString(16)).slice(-4);
+	var rDX_str = 'rDX: 0x' + ('0000' + rDX.toString(16)).slice(-4);
+	
+	var rSI_str = 'rSI: 0x' + ('0000' + rSI.toString(16)).slice(-4);
+	var rDI_str = 'rDI: 0x' + ('0000' + rDI.toString(16)).slice(-4);
+	var rBP_str = 'rBP: 0x' + ('0000' + rBP.toString(16)).slice(-4);
+	var rSP_str = 'rSP: 0x' + ('0000' + rSP.toString(16)).slice(-4);
+	
+	var rCS_str = 'rCS: 0x' + ('0000' + rCS.toString(16)).slice(-4);
+	var rDS_str = 'rDS: 0x' + ('0000' + rDS.toString(16)).slice(-4);
+	var rES_str = 'rES: 0x' + ('0000' + rES.toString(16)).slice(-4);
+	var rSS_str = 'rSS: 0x' + ('0000' + rSS.toString(16)).slice(-4);
+	
+	var rIP_str = 'rIP: 0x' + ('0000' + rIP.toString(16)).slice(-4);
+	var rST_str = 'rST: 0x' + ('0000' + rST.toString(16)).slice(-4);
+	
+	console.log('--------------------');
+	console.log(rAX_str + ' ' + rBX_str + ' ' + rCX_str + ' ' + rDX_str);
+	console.log(rSI_str + ' ' + rDI_str + ' ' + rBP_str + ' ' + rSP_str);
+	console.log(rCS_str + ' ' + rDS_str + ' ' + rES_str + ' ' + rSS_str);
+	console.log(rIP_str + ' ' + rST_str);
+	console.log('--------------------');
 }
 
 // Test the status register.
